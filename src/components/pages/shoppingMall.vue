6ccf0b5417c94b4287e1d4ce9ca83386f64d9152
chenglin <837460387@qq.com>
2019-03-11 10:51:31 +0800

    轮播图

--- src/components/pages/shoppingMall.vue
+++ src/components/pages/shoppingMall.vue
@@ -13,17 +13,41 @@
                 </van-col>
             </van-row>
         </div>
+        <div class="swipe-area">
+            <van-swipe :autoplay="3000">
+                <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index">
+                    <img :src="banner.imageUrl">
+                </van-swipe-item>
+            </van-swipe>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         data() {
             return {
                 msg: 'Shopping Mall',
-                locationImg: require('../../assets/images/location.png')
+                locationImg: require('../../assets/images/location.png'),
+                bannerPicArray:[
+                    {imageUrl:'https://aecpm.alicdn.com/simba/img/TB1CWf9KpXXXXbuXpXXSutbFXXX.jpg_q50.jpg'},
+                    {imageUrl:'https://aecpm.alicdn.com/simba/img/TB14ab1KpXXXXclXFXXSutbFXXX.jpg_q50.jpg'},
+                    {imageUrl:'https://aecpm.alicdn.com/simba/img/TB1CWf9KpXXXXbuXpXXSutbFXXX.jpg_q50.jpg'},
+                ],
+                categroy: []
             }
         },
+        created() {
+            axios({
+                url: "https://www.easy-mock.com/mock/5c85c410a37c3b12bb906f52/index/index",
+                method: "get"
+            }).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
     }
 </script>
 
@@ -32,6 +56,7 @@
         height: 2.2rem;
         background-color: #e5017d;
         line-height: 2.2rem;
+        overflow: hidden;
     }
     .search-icon {
         height: 2rem;
@@ -47,5 +72,10 @@
         border-bottom: 1px solid #fff;
         background-color: #e5017d;
         color: #fff;
+    }
+    .swipe-area {
+        clear: both;
+        max-height: 15rem;
+        overflow: hidden;
     }
 </style>
