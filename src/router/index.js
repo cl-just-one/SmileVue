e7d491b3dc9686d1817bc09dc6d72d44dc84e8c0
chenglin <837460387@qq.com>
2019-03-17 15:28:55 +0800

    register code

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ShoppingMall from '@/components/pages/shoppingMall'
+import Register from '@/components/pages/register'
 
 Vue.use(Router)
 
@@ -9,5 +10,9 @@ export default new Router({
         path: '/',
         name: 'ShoppingMall',
         component: ShoppingMall
+    }, {
+        path: '/register',
+        name: 'Register',
+        component: Register
     }]
 })
